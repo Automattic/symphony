6c019d4f29246e4834608ddb67df8129b1265983
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2026-05-08 16:25:49 +0800

    fix: keep upstream workflow and secret-key changes

--- elixir/test/test_helper.exs
+++ elixir/test/test_helper.exs
@@ -2,6 +2,11 @@ ExUnit.start()
 Code.require_file("support/snapshot_support.exs", __DIR__)
 Code.require_file("support/test_support.exs", __DIR__)
 
+System.put_env(
+  "SYMPHONY_SECRET_KEY_BASE",
+  System.get_env("SYMPHONY_SECRET_KEY_BASE", String.duplicate("s", 64))
+)
+
 audit_dir = Path.join(System.tmp_dir!(), "symphony-elixir-test-audit-#{System.unique_integer([:positive])}")
 Application.put_env(:symphony_elixir, :audit_log_dir, audit_dir)
 ExUnit.after_suite(fn _results -> File.rm_rf(audit_dir) end)
